d85b0720b618f26b808a7d502719b01b17f92897
Mrugesh Mohapatra <noreply@mrugesh.dev>
2026-07-20 18:39:58 +0530

    feat(keybinds): ctrl-alt WM scheme, relocate terminal nav

--- dot_config/nvim/lua/plugins/smart-splits.lua
+++ dot_config/nvim/lua/plugins/smart-splits.lua
@@ -4,9 +4,9 @@ return {
   config = function()
     local ss = require("smart-splits")
     ss.setup()
-    vim.keymap.set("n", "<C-M-h>", ss.move_cursor_left, { desc = "Move to left pane" })
-    vim.keymap.set("n", "<C-M-j>", ss.move_cursor_down, { desc = "Move to lower pane" })
-    vim.keymap.set("n", "<C-M-k>", ss.move_cursor_up, { desc = "Move to upper pane" })
-    vim.keymap.set("n", "<C-M-l>", ss.move_cursor_right, { desc = "Move to right pane" })
+    vim.keymap.set("n", "<A-H>", ss.move_cursor_left, { desc = "Move to left pane" })
+    vim.keymap.set("n", "<A-J>", ss.move_cursor_down, { desc = "Move to lower pane" })
+    vim.keymap.set("n", "<A-K>", ss.move_cursor_up, { desc = "Move to upper pane" })
+    vim.keymap.set("n", "<A-L>", ss.move_cursor_right, { desc = "Move to right pane" })
   end,
 }
