0d3307f7def317c0163684c816715d8eca34c364
Mrugesh Mohapatra <noreply@mrugesh.dev>
2026-08-01 00:28:47 +0530

    fix(zsh): resolve fnm default node in non-interactive shells

--- dot_config/zsh/path.zsh
+++ dot_config/zsh/path.zsh
@@ -12,6 +12,7 @@ _mrgsh_compose_path() {
 	_mrgsh_path_prepend \
 		"$HOMEBREW_PREFIX/sbin" \
 		"$HOMEBREW_PREFIX/bin" \
+		"${XDG_DATA_HOME:-$HOME/.local/share}/fnm/aliases/default/bin" \
 		"$GOPATH/bin" \
 		"$HOME/.cargo/bin" \
 		"$HOME/.local/bin" \
